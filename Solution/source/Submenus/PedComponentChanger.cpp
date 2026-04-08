1e1eb7f5af778980eeaaa0a3e812e0686064f531
greygxds <155662346+greygxds@users.noreply.github.com>
2026-04-08 04:33:44 +0200

    Revert "Decal overlay preview"

--- Solution/source/Submenus/PedComponentChanger.cpp
+++ Solution/source/Submenus/PedComponentChanger.cpp
@@ -30,7 +30,6 @@
 #include "..\Util\ExePath.h"
 #include "..\Util\FileLogger.h"
 #include "..\Util\StringManip.h"
-#include "..\Util\keyboard.h"
 
 #include "..\Menu\FolderPreviewBmps.h"
 #include "..\Submenus\PedModelChanger.h"
@@ -585,18 +584,6 @@ namespace sub
 	{
 		std::map<Ped, std::vector<PedDecalValue>> vPedsAndDecals;
 
-		bool g_tattooPreviewMode = false;
-		const NamedPedDecal* g_previewTattoo = nullptr;
-
-		void ClearPreviewTattoo()
-		{
-			if (g_previewTattoo && DOES_ENTITY_EXIST(g_Ped1))
-			{
-				g_previewTattoo->Remove(g_Ped1);
-				g_previewTattoo = nullptr;
-			}
-		}
-
 		bool NamedPedDecal::IsOnPed(GTAentity ped) const
 		{
 			auto it = vPedsAndDecals.find(ped.Handle());
@@ -745,42 +732,11 @@ namespace sub
 		{
 			GTAentity ped = g_Ped1;
 
-			if (Menu::OnSubBack == nullptr)
-			{
-				Menu::OnSubBack = []
-				{
-					PedDecals::g_tattooPreviewMode = false;
-					PedDecals::ClearPreviewTattoo();
-				};
-			}
-
-			if (get_key_pressed(VirtualKey::B))
-			{
-				g_tattooPreviewMode = !g_tattooPreviewMode;
-				if (!g_tattooPreviewMode)
-				{
-					ClearPreviewTattoo();
-				}
-			}
-
 			AddTitle(selectedZone->first);
 
 			for (const auto& decal : selectedZone->second)
 			{
-				
-				bool isHovered = (*Menu::currentopATM == Menu::printingop + 1);
 				bool bDecalPressedApply = false, bDecalPressedRemove = false;
-
-				if (g_tattooPreviewMode && isHovered)
-				{
-					if (g_previewTattoo != &decal)
-					{
-						ClearPreviewTattoo();
-						decal.Apply(ped);
-						g_previewTattoo = &decal;
-					}
-				}
-
 				AddTickol(decal.caption, decal.IsOnPed(ped), bDecalPressedApply, bDecalPressedRemove, TICKOL::TATTOOTHING);
 				if (bDecalPressedApply)
 				{
@@ -791,8 +747,6 @@ namespace sub
 					decal.Remove(ped);
 				}
 			}
-
-			Menu::add_IB(VirtualKey::B, g_tattooPreviewMode ? "Preview: ON " : "Preview: OFF ");
 
 		}
 		void OpenSubDecals()
@@ -1178,7 +1132,7 @@ namespace sub
 				}
 
 				SET_PED_HEAD_OVERLAY(ped.Handle(), overlayIndex, overlayValue, currentOverlayData.opacity);
-				ApplyHeadOverlayTint(ped, overlayIndex, colourType, currentOverlayData.colour, currentOverlayData.colourSecondary);
+				ApplyHeadOverlayTint(ped, overlayIndex, colourType, currentOverlayData.colour = -1, currentOverlayData.colourSecondary = -1);
 			}
 
 			if (overlayMinus)
@@ -1193,7 +1147,7 @@ namespace sub
 				}
 
 				SET_PED_HEAD_OVERLAY(ped.Handle(), overlayIndex, overlayValue, currentOverlayData.opacity);
-				ApplyHeadOverlayTint(ped, overlayIndex, colourType, currentOverlayData.colour, currentOverlayData.colourSecondary);
+				ApplyHeadOverlayTint(ped, overlayIndex, colourType, currentOverlayData.colour = -1, currentOverlayData.colourSecondary = -1);
 			}
 
 			// OPACITY
